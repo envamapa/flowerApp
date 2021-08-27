8878ffe0b945724202df0f2efd0eb60477282aa5
Enya Martínez <enya.martinez@trivago.com>
2021-08-27 11:46:49 -0500

    Create add button

--- app/src/main/java/com/envamapa/custom/floweapp/MainActivity.kt
+++ app/src/main/java/com/envamapa/custom/floweapp/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.envamapa.custom.floweapp
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.AndroidPaint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.envamapa.custom.floweapp.entities.Flower
 import com.envamapa.custom.floweapp.ui.theme.FloweAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,13 +34,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FloweAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                FlowerCard(flower = Flower("Angle", "$570.00"))
             }
         }
     }
+}
+
+@Composable
+fun FlowerCard(flower: Flower) {
+    Card(
+        shape = RoundedCornerShape(14.dp),
+        backgroundColor = Color.White,
+        modifier = Modifier
+            .padding(10.dp)
+            .width(180.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Column {
+                Text(
+                    text = flower.name,
+                    style = TextStyle(
+                        color = Color.Gray,
+                        fontSize = 16.sp
+                    )
+                )
+                Text(
+                    text = flower.price,
+                    style = TextStyle(
+                        color = Color.Green,
+                        fontSize = 16.sp
+                    )
+                )
+            }
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.background(
+                    color = Color.Green,
+                    shape = RoundedCornerShape(10.dp)
+                )
+            ) {
+                Icon(
+                    Icons.Default.Add,
+                    contentDescription = "Add",
+                    tint = Color.White
+                )
+            }
+        }
+    }
+}
+
+fun imageResource(id: Int): Any {
+
 }
 
 @Composable
@@ -33,6 +98,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     FloweAppTheme {
-        Greeting("Android")
+        FlowerCard(flower = Flower("Angle", "$570.00"))
     }
 }
---
+++ app/src/main/java/com/envamapa/custom/floweapp/entities/Flower.kt
@@ -0,0 +1,7 @@
+package com.envamapa.custom.floweapp.entities
+
+data class Flower(
+    val name: String,
+    val price: String,
+    val image: Int
+)
